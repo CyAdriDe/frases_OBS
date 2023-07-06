c2f0a858cc25179caf980e1eaae7ac871fc4645b
Adrián Soria Montoya <cyadride@gmail.com>
2023-07-06 19:20:23 +0200

    Puesta a punto

--- google_sheets.py
+++ google_sheets.py
@@ -3,8 +3,8 @@ import time
 import csv
 
 def read_sheet():
-	SHEET_ID = '1R20ADzvvkU7lHqtCAURIQUq4uDXQ_3ZpPkhPIIRhIkw'
-	SHEET_NAME = 'Form1'
+	SHEET_ID = '1F7Q3YZZ8vzKq5ifFmp04NrrXKJDMzTDrXNmp-WcVsQ4'
+	SHEET_NAME = 'Sheet1'
 	url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 	df = pd.read_csv(url)
 	df.to_csv('llista_sheets.csv', index=False, encoding='utf-8')
@@ -15,7 +15,7 @@ def create_list():
 		frases = []
 		reader = csv.reader(f, delimiter = ",")
 		for row in reader:
-			frases.append(row[1])
+			frases.append(row[0])
 	frases = frases[::-1]
 	frases.pop() # Eliminem el títol de la columna
 		
